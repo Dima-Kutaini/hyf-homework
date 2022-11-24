daa5148a96ff073ce2852f5892bd70506b4a3efb
Dima-Kutaini <111286582+Dima-Kutaini@users.noreply.github.com>
2022-11-24 21:29:48 +0100

    Revert "Javascript1 homework"

--- javascript/javascript1/week1/Age.js
+++
@@ -1,6 +0,0 @@
-/* Age-ify (A future age calculator)*/
-let yearOfBirth= 1984;
-let yearFuture = 2027;
-let myAge= yearFuture - yearOfBirth;
-console.log("you will be"+43 + " years old in 2027" );
-
