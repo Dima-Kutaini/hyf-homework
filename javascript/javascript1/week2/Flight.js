cef9710476c683b812f193da8b5007b4a11b59aa
Dima-Kutaini <deemakutainy@gmail.com>
2022-12-03 14:43:52 +0100

    Add full name function

---
+++ javascript/javascript1/week2/Flight.js
@@ -0,0 +1,19 @@
+
+// Flight booking fullname function 
+ 
+
+function getFullname( firstName, surName){
+    
+
+    return  firstName + surName ; 
+
+};
+console.log( "Dima " ,"Kutaini" ); 
+
+let fullName1 = " sima";
+let fullName2 = "kan"; 
+
+console.log (fullName1 ,fullName2); 
+
+
+
